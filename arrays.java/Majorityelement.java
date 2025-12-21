886292a4ec2a94fbbea09e96964fd00f6586cbc8
Smitha <smitha.t@cmr.edu.in>
2025-12-22 00:54:44 +0530

    Added Majorityelement file

---
+++ arrays.java/Majorityelement.java
@@ -0,0 +1,58 @@
+
+
+
+class Majorityelement {
+   
+    public int majorityElement(int[] nums) {
+    
+        int n = nums.length;
+    
+        int cnt = 0;
+        
+       
+        int el = 0;
+        
+        
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if (el == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        
+     
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) {
+                cnt1++;
+            }
+        }
+        
+       
+        if (cnt1 > (n / 2)) {
+            return el;
+        }
+        
+        
+        return -1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        
+       
+        Majorityelement sol = new Majorityelement();
+
+       
+        int ans = sol.majorityElement(arr);
+        
+        
+        System.out.println("The majority element is: " + ans);
+    }
+}
