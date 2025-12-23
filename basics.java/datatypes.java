332bfa7aa2b2fd0461b8511f1fa3e2de0d31c878
Smitha <smitha.t@cmr.edu.in>
2025-12-24 00:20:35 +0530

    Added new practise problem

---
+++ basics.java/datatypes.java
@@ -0,0 +1,17 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int i = 101110;
+        float f = 10.1f;
+        byte b = 42;
+        double d = 10.1456;
+        char c = 'A';
+        short s = 1023;
+        double result= (f*b)+(i/c)+(d-s);
+        System.out.println((f*b)+" "+(i/c)+" "+(d-s));
+        System.out.println(result);
+
+
+        
+    }
+    
+}
