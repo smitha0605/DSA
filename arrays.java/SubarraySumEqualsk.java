0ac8e01b61965f3df39a6483a37ffedb3c75d79a
Smitha <smitha.t@cmr.edu.in>
2026-01-06 23:54:36 +0530

    Add

---
+++ arrays.java/SubarraySumEqualsk.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class SubarraySumEqualsk {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        System.out.println(count);
+    }
+}
