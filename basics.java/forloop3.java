af9ed4cc6d93c50e384b6858b4303750adb0760d
Smitha <smitha.t@cmr.edu.in>
2026-01-08 17:40:52 +0530

    Update

--- basics.java/forloop3.java
+++ basics.java/forloop3.java
@@ -3,7 +3,7 @@ public class forloop3 {
     public static void main(String[] args){
        
       
-        for(int i = 10 ;i >=1 ;i --){
+        for(int i = 6 ;i >=1 ;i --){
             System.out.println(i + " ");
         }
     }
