cc0e27d112f2e49d58ec3e67e2c59a7b6f4549cd
Smitha <smitha.t@cmr.edu.in>
2026-01-11 23:34:45 +0530

    Solve Max consecutive problem

---
+++ arrays.java/MaxConsecutiveOnes.java
@@ -0,0 +1,19 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+
+        int max = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        System.out.println("Maximum consecutive 1s: " + max);
+    }
+}
