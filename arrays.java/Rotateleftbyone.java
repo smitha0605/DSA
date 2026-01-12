b50efc05e10df678213c0adb9eb75d04a82c56c4
Smitha <smitha.t@cmr.edu.in>
2026-01-12 23:09:26 +0530

    Add

---
+++ arrays.java/Rotateleftbyone.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Rotateleftbyone {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+
+        int first = nums[0];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+
+        nums[nums.length - 1] = first;
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
