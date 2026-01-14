703ece113f524f533365f4d676d93c8887372f02
Smitha <smitha.t@cmr.edu.in>
2026-01-15 01:39:48 +0530

    Add

---
+++ basics.java/printindex.java
@@ -0,0 +1,9 @@
+public class printindex {
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4};
+        for (int i =0; i<arr.length; i ++){
+            System.out.println("Index: " + i + " Value: " + arr[i]  );
+        }
+    }
+    
+}
