75e2536fa3a0e8a717374bddb6f5038db155b555
Smitha <smitha.t@cmr.edu.in>
2026-01-16 23:37:19 +0530

    Add linear search sum

---
+++ arrays.java/linearSearch.java
@@ -0,0 +1,25 @@
+ public class linearSearch {
+
+    public static int findFirstIndex(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;   
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 3, 4, 5, 3};
+        int target = 3;
+
+        int result = findFirstIndex(nums, target);
+        System.out.println(result);
+    }
+}
+ 
+    
+
