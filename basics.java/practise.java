951dd51af7efd83924a45819774e5fc510bbef85
Smitha <smitha.t@cmr.edu.in>
2026-01-20 22:33:46 +0530

    Add

---
+++ basics.java/practise.java
@@ -0,0 +1,11 @@
+public class practise {
+    public static void main(String[] args) {
+        System.out.println(add (10,20));
+        System.out.println(add (13,20));
+
+    }
+    public static int add(int a , int b){
+        return a + b;
+    }
+    
+}
