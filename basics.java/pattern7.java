96fa7b39a5ed849480c7b5aff4a2cdfd9a4ff109
Smitha <smitha.t@cmr.edu.in>
2026-01-26 23:12:29 +0530

    modify

--- basics.java/pattern7.java
+++ basics.java/pattern7.java
@@ -7,11 +7,11 @@ public class pattern7 {
 
      
 
-       for(int i=n; i>=1; i--) {
+       for(int i=1; i<=n; i++) {
 
-           for(int j=1; j<=i; j++) {
+           for(int j=1; j<=n-i+1; j++) {
 
-               System.out.print(j);
+               System.out.print(j+" ");
 
            }
 
