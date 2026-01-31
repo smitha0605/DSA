52d58e5b8cb11f0c0123ce75a94f9ebf5aa60060
Smitha <smitha.t@cmr.edu.in>
2026-01-31 23:46:28 +0530

    Add four sum

---
+++ arrays.java/foursum.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class foursum {
+    public static void main(String[] args) {
+        int[] nums = {1, -2, 3, 5, 7, 9};
+        int target = 7;
+
+        System.out.println(fourSum(nums, target));
+    }
+
+    static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1, right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
