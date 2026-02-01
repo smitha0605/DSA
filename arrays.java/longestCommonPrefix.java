e9c75ff85ab123910535d34914ae8681df75193f
Smitha <smitha.t@cmr.edu.in>
2026-02-01 23:54:03 +0530

    Modify

--- arrays.java/longestCommonPrefix.java
+++ arrays.java/longestCommonPrefix.java
@@ -6,8 +6,9 @@ public class longestCommonPrefix {
     }
 
     static String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
         String prefix = strs[0];
 
         for (int i = 1; i < strs.length; i++) {
