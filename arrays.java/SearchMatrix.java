66114ae5394fd7ddf2fa90652d9da0698baafd45
Smitha <smitha.t@cmr.edu.in>
2026-02-21 22:47:10 +0530

    Solve matrix search using binary search

--- arrays.java/SearchMatrix.java
+++ arrays.java/SearchMatrix.java
@@ -32,7 +32,7 @@ public class SearchMatrix{
     public static void main(String[] args) {
 
         int[][] matrix = {
-            {1, 3, 5, 7},
+            {1, 3, 6, 7},
             {10, 11, 16, 20},
             {23, 30, 34, 60}
         };
