06bba2c514af7f22b298d54e691bcc5585d79fba
Smitha <smitha.t@cmr.edu.in>
2026-03-04 23:15:05 +0530

    Add

--- arrays.java/Majorityelement.java
+++ arrays.java/Majorityelement.java
@@ -43,7 +43,7 @@
 
 
     public static void main(String[] args) {
-        int[] arr = {2, 2, 1, 1, 2, 2, 2};
+        int[] arr = {2, 2, 1, 1, 2, 2, 2,3};
         
        
         Majorityelement sol = new Majorityelement();
