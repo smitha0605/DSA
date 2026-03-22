fab37473767a7d3fc98b282dfd3fcdf352c34b5b
Smitha <smitha.t@cmr.edu.in>
2026-03-22 23:06:31 +0530

    solve

---
+++ strings.java/IsomorphicStrings.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class IsomorphicStrings {
+
+    public static boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (map1.containsKey(c1)) {
+                if (map1.get(c1) != c2) return false;
+            } else {
+                map1.put(c1, c2);
+            }
+
+            if (map2.containsKey(c2)) {
+                if (map2.get(c2) != c1) return false;
+            } else {
+                map2.put(c2, c1);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("egg", "add"));   
+        System.out.println(isIsomorphic("f11", "b23"));  
+    }
+}
