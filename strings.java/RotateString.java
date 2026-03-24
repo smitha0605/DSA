9b09b592363e2d01be38209824e64d9ddae7b9b4
Smitha <smitha.t@cmr.edu.in>
2026-03-24 22:58:28 +0530

    solve

---
+++ strings.java/RotateString.java
@@ -0,0 +1,20 @@
+public class RotateString {
+
+    public static boolean rotateString(String s, String goal) {
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        String temp = s + s;
+
+        return temp.contains(goal);
+    }
+
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+
+        System.out.println(rotateString(s, goal)); 
+    }
+}
